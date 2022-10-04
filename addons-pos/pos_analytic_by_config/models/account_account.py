e511ebc062d28adc745b5b47c76146b085654347
WaseemShaikh <wassim.sheikh@gmail.com>
2022-10-04 21:10:05 +0530

    pos journal entry, AA changes

---
+++ addons-pos/pos_analytic_by_config/models/account_account.py
@@ -0,0 +1,11 @@
+from odoo import api, models
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    def _check_account_pnl(self):
+        """
+            Checks if account is under profit and loss, so AA can be added to its JE
+        """
+        self.ensure_one()
+        return self.user_type_id.internal_group in ('income', 'expense') and True or False
